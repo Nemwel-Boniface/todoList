ddc4ef3096c9aac8767f55c1969ed453a3a7803a
Nemwel Boniface <nemwelnyandoro20@gmail.com>
2022-02-15 21:29:14 +0300

    Update Project files

--- src/index.js
+++ src/index.js
@@ -1,14 +1,41 @@
 import _ from 'lodash';
 import './style.css';
 
-function component() {
-  const element = document.createElement('div');
+const id = 0;
 
-  // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
+const tasks = [
+  {
+    description: 'Buy groceries',
+    completed: false,
+    index: 0,
+  },
+  {
+    description: 'Clean the House',
+    completed: true,
+    index: 1,
+  },
+  {
+    description: 'Water the Flowers',
+    completed: true,
+    index: 1,
+  },
+]
 
-  return element;
+const taskWrapper = document.querySelector('.activities');
+function displayTasks() {
+  taskWrapper.innerHTML = '';
+  for(let i = 0; i < tasks.length; i += 1) {
+    taskWrapper.innerHTML += `
+    <form class="atask">
+      <input type="checkbox" id="${tasks[i].index}" name="task" value="task">
+      <label for="${tasks[i].index}">${tasks[i].description}</label>
+      <i class="fa fa fa-times"></i><br>
+    </form>
+    `;
+  }
+
 }
-
-document.body.appendChild(component());
+{/* <p>${tasks[i].description}</p> */}
+window.addEventListener('load', () => {
+  displayTasks();
+})
