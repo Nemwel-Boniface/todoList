c9a04ff1e920c34f359d7113e0ae29e957952ef5
Nemwel Boniface <nemwelnyandoro20@gmail.com>
2022-02-18 08:14:59 +0300

    Update include display to screen functionality

--- src/index.js
+++ src/index.js
@@ -19,6 +19,34 @@ function getFromLocalStorage() {
   return tasks;
 }
 
+
+function displayTasks() {
+  taskWrapper.innerHTML = '';
+  let mylocal = getFromLocalStorage();
+
+  mylocal.forEach((tsk, index) => {
+    let li = document.createElement('li');
+    let checkbox = document.createElement('input');
+    checkbox.setAttribute('type','checkbox');
+    if(tasks.checked){
+      checkbox.setAttribute('checked','checked');
+    }
+    checkbox.addEventListener('change',()=>onchange(tasks));
+    // li.appendChild(checkbox);
+    const taskDesc = document.createElement('span');
+    taskDesc.innerHTML = tsk.description;
+    // li.appendChild(taskDesc);
+    // const editbtn = document.createElement('button');
+    // editbtn.onclick = () => editDescription(todos);
+    const icon = document.createElement('i');
+    icon.classList.add('fas','fa-ellipsis-v');
+    // button.appendChild(icon);
+    // li.appendChild(icon);
+    li.append(checkbox, taskDesc, icon);
+    taskWrapper.appendChild(li);
+  })
+}
+
 function addToTasks() {
   tasks.push({
     checked: false,
